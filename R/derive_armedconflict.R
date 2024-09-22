c4df55ddae3af3bb62776aaa61411d84761eead7
felixkaiho <fekaiho@gmail.com>
2024-09-21 22:30:35 -0400

    Derive the binary armed conflict variable

---
+++ R/derive_armedconflict.R
@@ -0,0 +1,21 @@
+#-----------------------------------
+# Author: Felix Ho
+# Last updated: 2024-9-21
+#-----------------------------------
+# In-Class Assignment 3 (2024/9/23)
+
+library(here)
+library(tidyverse)
+
+# Add 1 to year because the armed conflict variable was lagged by a year in the analysis
+
+rawconflict <- read.csv(here("original", "conflictdata.csv"), header = TRUE)
+subconflict <- select(rawconflict, ISO, year, best)
+
+armedconflictdata <- subconflict %>%
+  group_by(ISO, year) %>%
+  summarise(deaths = sum(best)) %>%
+  mutate(Armed_conflict = if_else(deaths < 25, 0, 1)) %>%
+  mutate(year = year + 1) %>%
+  rename(Year = year) %>%
+  subset(select = -deaths)
