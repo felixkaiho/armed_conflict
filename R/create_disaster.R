a71fdddbf2f27d8a9f0b7175ff95226f8b417970
felixkaiho <fekaiho@gmail.com>
2024-09-23 22:08:08 -0400

    Revise code

--- R/create_disaster.R
+++ R/create_disaster.R
@@ -1,6 +1,6 @@
 #-----------------------------------
 # Author: Felix Ho
-# Last updated: 2024-9-21
+# Last updated: 2024-9-23
 # What: Read in raw data,
 #       subset data,
 #       convert to wide format
@@ -10,16 +10,18 @@ library(tidyverse)
  
 rawdisaster <- read.csv(here("original", "disaster.csv"), header = TRUE)
 
-# subset data
+# Subset data.
 
-subdisaster <- filter(rawdisaster, Year %in% 2000:2019, Disaster.Type == "Earthquake" | Disaster.Type == "Drought")
-subdisaster <- select(subdisaster, Year, ISO, Disaster.Type)
-subdisaster <- subdisaster %>%
+subdisaster <- filter(rawdisaster, Year >= 2000 & Year <= 2019, 
+                      Disaster.Type == "Earthquake" | 
+                        Disaster.Type == "Drought") %>%
+  select(Year, ISO, Disaster.Type) %>%
+  rename(year = Year) %>%
   mutate(drought = if_else(Disaster.Type == "Drought", 1, 0)) %>%
   mutate(earthquake = if_else(Disaster.Type == "Earthquake", 1, 0))
 
-# convert to wide format
+# Convert to wide format.
 
 cleandisasterdata <- subdisaster %>%
-  group_by(Year, ISO) %>%
+  group_by(year, ISO) %>%
   summarise(drought = max(drought), earthquake = max(earthquake))
