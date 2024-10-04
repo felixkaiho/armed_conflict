e792a2c82f1c67c59f1310e72a02330a2cbb1b3b
felixkaiho <fekaiho@gmail.com>
2024-10-04 17:35:43 -0400

    Edit explanation of the code

--- R/derive_armedconflict.R
+++ R/derive_armedconflict.R
@@ -7,10 +7,20 @@
 library(here)
 library(tidyverse)
 
-# The binary armed conflict variable indicates the presence of conflict for each
-# country-year observation (0 = no, < 25 battle-related deaths; 1 = yes, >= 25
-# battle-related deaths). Add 1 to year because the armed conflict variable was
-# lagged by a year in the analysis.
+# conflictdata.csv has already been pre-processed and only contains relevant
+# variables and years of observations to create the binary armed conflict
+# variable.
+
+# Derive the binary armed conflict variable that was used as the primary
+# exposure in the paper. The binary armed conflict variable indicates the
+# presence of conflict for each country-year observation (0 = no, < 25
+# battle-related deaths; 1 = yes, >= 25 battle-related deaths). 
+
+# Some countries have multiple conflicts per year, but we want the final data to
+# only include the indicator of whether the country had a conflict that year.
+
+# Add 1 to year because the armed conflict variable was lagged by a year in the
+# analysis.
 
 rawconflict <- read.csv(here("original", "conflictdata.csv"), header = TRUE)
 subconflict <- select(rawconflict, ISO, year, best)
